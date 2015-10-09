7a7e0a412635d5a1bfa55527723df3e1a12ec1b2
tyage <namatyage@gmail.com>
2015-10-09 20:05:05 +0900

    update mactex and path becomes /Library/TeX/texbin

--- Casks/mactex.rb
+++ Casks/mactex.rb
@@ -1,9 +1,9 @@
 cask :v1 => 'mactex' do
-  version '20150613'
+  version '20150930'
   sha256 'c5f5b0fd853a17dab6e844fb5e893804af78d938fa18ee94ec3b257611a95c12'
 
   # ctan.org is the official download host per the vendor homepage
-  url "http://mirror.ctan.org/systems/mac/mactex/mactex-#{version}.pkg"
+  url "http://tug.org/cgi-bin/mactex-download/MacTeX.pkg"
   name 'MacTeX'
   homepage 'https://www.tug.org/mactex/'
   license :oss
@@ -20,6 +20,6 @@ cask :v1 => 'mactex' do
                          '/etc/paths.d/TeX',
                         ]
   caveats do
-    zsh_path_helper '/usr/texbin'
+    zsh_path_helper '/Library/TeX/texbin'
   end
 end
